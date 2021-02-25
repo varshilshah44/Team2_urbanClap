8def94405d6900cb56b9601215f3c9dd2cc924a6
DJ911 <dharmaraj.jadeja@solulab.co>
2021-02-25 17:57:54 +0530

    DJ schema

---
+++ models/serviceModel.js
@@ -0,0 +1,55 @@
+const mongoose = require('../dbconnection');
+
+const serviceSchema = new mongoose.Schema({
+    categoryId: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'category',
+        required: true
+    },
+    serviceName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    servicePrice: {
+        type: Number,
+        validate: [/^[1-9]\d*$/, 'Please enter valid price'],
+        required: [true, 'Price is required']
+    },
+    serviceDescription: {
+        type: String        // If required use min max length
+    },
+    serviceTime: {
+        type: String,
+        required: true
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    vendorId: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'user'
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt: Date
+}, { collection: 'service' })
+
+exports.service = mongoose.model('service', serviceSchema);
+
+/**
+ * 2) services
+
+- categoryId :
+- serviceName : "Ac Re..",
+- servicePrice : 20
+- serviceDescription :
+####- serviceTime:######
+####- active: Boolean######
+- vendorId : array of vendorIds who provide this service , vendorId means userId
+ */
